cba6e6a8830c79153aa9101d639843bd043d6351
notdijon <61089563+notdijon@users.noreply.github.com>
2020-02-15 20:42:37 -0500

    ignore non snake case warnings in bindgen-generated code

--- src/ffi/bindings.rs
+++ src/ffi/bindings.rs
@@ -2,6 +2,7 @@
 
 #![allow(dead_code)]
 #![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
 #![allow(missing_docs)]
 use libc::time_t;
 
--- src/ffi/generate.sh
+++ src/ffi/generate.sh
@@ -5,5 +5,6 @@ bindgen hexchat-plugin.h -o bindings.rs \
 --blacklist-type time_t \
 --raw-line "#![allow(dead_code)]" \
 --raw-line "#![allow(non_camel_case_types)]" \
+--raw-line "#![allow(non_snake_case)]" \
 --raw-line "#![allow(missing_docs)]" \
 --raw-line "use libc::time_t;" \
