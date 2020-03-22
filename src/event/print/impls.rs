f6aed689ac14d6c6b2054cb7a1bf579cad4c4777
notdijon <61089563+notdijon@users.noreply.github.com>
2020-03-22 15:20:51 -0400

    fmt

--- src/event/print/impls.rs
+++ src/event/print/impls.rs
@@ -1,7 +1,7 @@
 print_event!(AddNotify, "Add Notify", "`%C18*%O$t%C18$1%O added to notify list.`", 0: "Nickname", 1: "Server Name", 2: "Network");
 print_event!(BanList, "Ban List", "`%C22*%O$t%C22$1%O: %C18$2%O on %C24$4%O by %C26$3%O`", 0: "Channel", 1: "Banmask", 2: "Who set the ban", 3: "Ban time");
 print_event!(Banned, "Banned", "`%C22*%O$tCannot join %C22$1 %O(%C20You are banned%O).`", 0: "Channel Name");
-print_event!(Beep, "Beep", "``", );
+print_event!(Beep, "Beep", "``",);
 print_event!(CapabilityAcknowledgement, "Capability Acknowledgement", "`%C29*%O$tCapabilities acknowledged: %C29$2%O`", 0: "Server Name", 1: "Acknowledged Capabilities");
 print_event!(CapabilityDeleted, "Capability Deleted", "`%C29*%O$tCapabilities removed: %C29$2%O`", 0: "Server Name", 1: "Removed Capabilities");
 print_event!(CapabilityList, "Capability List", "`%C23*%O$tCapabilities supported: %C29$2%O`", 0: "Server Name", 1: "Server Capabilities");
@@ -17,7 +17,11 @@ print_event!(ChannelDevoice, "Channel DeVoice", "`%C22*%O$t%C26$1%O removes voic
 print_event!(ChannelExempt, "Channel Exempt", "`%C22*%O$t%C26$1%C sets exempt on %C18$2%O`", 0: "The nick of the person who did the exempt", 1: "The exempt mask");
 print_event!(ChannelHalfOperator, "Channel Half-Operator", "`%C22*%O$t%C26$1%O gives channel half-operator status to %C18$2%O`", 0: "The nick of the person who has been halfop'ed", 1: "The nick of the person who did the halfop'ing");
 print_event!(ChannelInvite, "Channel INVITE", "`%C22*%O$t%C26$1%C sets invite exempt on %C18$2%O`", 0: "The nick of the person who did the invite", 1: "The invite mask");
-print_event!(ChannelList, "Channel List", "`%UChannel          Users   Topic`", );
+print_event!(
+    ChannelList,
+    "Channel List",
+    "`%UChannel          Users   Topic`",
+);
 print_event!(ChannelMessage, "Channel Message", "`%C18%H<%H$4$1%C18%H>%H%O$t$2`", 0: "Nickname", 1: "The text", 2: "Mode char", 3: "Identified text");
 print_event!(ChannelModeGeneric, "Channel Mode Generic", "`%C22*%O$t%C26$1%O sets mode %C24$2$3%O on %C22$4%O`", 0: "The nick of the person setting the mode", 1: "The mode's sign (+/-)", 2: "The mode letter", 3: "The channel it's being set on");
 print_event!(ChannelModes, "Channel Modes", "`%C22*%O$tChannel %C22$1%O modes: %C24$2`", 0: "Channel Name", 1: "Modes string");
@@ -35,7 +39,11 @@ print_event!(ChannelUnban, "Channel UnBan", "`%C22*%O$t%C26$1%O removes ban on %
 print_event!(ChannelUnquiet, "Channel UnQuiet", "`%C22*%O$t%C26$1%O removes quiet on %C18$2%O`", 0: "The nick of the person who did the unquiet'ing", 1: "The quiet mask");
 print_event!(ChannelUrl, "Channel Url", "`%C22*%O$tChannel %C22$1%O url: %C24$2`", 0: "Channel Name", 1: "URL");
 print_event!(ChannelVoice, "Channel Voice", "`%C22*%O$t%C26$1%O gives voice to %C18$2%O`", 0: "The nick of the person who did the voice'ing", 1: "The nick of the person who has been voice'ed");
-print_event!(Connected, "Connected", "`%C23*%O$tConnected. Now logging in.`", );
+print_event!(
+    Connected,
+    "Connected",
+    "`%C23*%O$tConnected. Now logging in.`",
+);
 print_event!(Connecting, "Connecting", "`%C23*%O$tConnecting to %C29$1%C (%C23$2:$3%O)`", 0: "Host", 1: "IP", 2: "Port");
 print_event!(ConnectionFailed, "Connection Failed", "`%C20*%O$tConnection failed (%C20$1%O)`", 0: "Error");
 print_event!(CtcpGeneric, "CTCP Generic", "`%C24*%O$tReceived a CTCP %C24$1%C from %C18$2%O`", 0: "The CTCP event", 1: "The nick of the person");
@@ -51,10 +59,18 @@ print_event!(DccChatOffering, "DCC CHAT Offering", "`%C24*%O$tOffering DCC CHAT 
 print_event!(DccChatReoffer, "DCC CHAT Reoffer", "`%C24*%O$tAlready offering CHAT to %C18$1%O`", 0: "Nickname", 1: "Server Name", 2: "Network");
 print_event!(DccConectionFailed, "DCC Conection Failed", "`%C20*%O$tDCC $1 connect attempt to %C18$2%O failed (%C20$3%O)`", 0: "DCC Type", 1: "Nickname", 2: "Error");
 print_event!(DccGenericOffer, "DCC Generic Offer", "`%C23*%O$tReceived '%C23$1%C' from %C18$2%O`", 0: "DCC String", 1: "Nickname");
-print_event!(DccHeader, "DCC Header", "`%C16,17 Type  To/From    Status  Size    Pos     File`", );
+print_event!(
+    DccHeader,
+    "DCC Header",
+    "`%C16,17 Type  To/From    Status  Size    Pos     File`",
+);
 print_event!(DccMalformed, "DCC Malformed", "`%C20*%O$tReceived a malformed DCC request from %C18$1%O.$a010%C23*%O$tContents of packet: %C23$2%O`", 0: "Nickname", 1: "The Packet");
 print_event!(DccOffer, "DCC Offer", "`%C24*%O$tOffering '%C24$1%O' to %C18$2%O`", 0: "Filename", 1: "Nickname", 2: "Pathname");
-print_event!(DccOfferNotValid, "DCC Offer Not Valid", "`%C23*%O$tNo such DCC offer.`", );
+print_event!(
+    DccOfferNotValid,
+    "DCC Offer Not Valid",
+    "`%C23*%O$tNo such DCC offer.`",
+);
 print_event!(DccRecvAbort, "DCC RECV Abort", "`%C23*%O$tDCC RECV '%C23$2%O' to %C18$1%O aborted.`", 0: "Nickname", 1: "Filename");
 print_event!(DccRecvComplete, "DCC RECV Complete", "`%C24*%O$tDCC RECV '%C23$1%O' from %C18$3%O complete %C30[%C24$4%O cps%C30]%O`", 0: "Filename", 1: "Destination filename", 2: "Nickname", 3: "CPS");
 print_event!(DccRecvConnect, "DCC RECV Connect", "`%C24*%O$tDCC RECV connection established to %C18$1 %C30[%O%C24$2%C30]%O`", 0: "Nickname", 1: "IP address", 2: "Filename");
@@ -75,10 +91,18 @@ print_event!(FoundIp, "Found IP", "`%C24*%O$tFound your IP: %C30[%C24$1%C30]%O`"
 print_event!(GenericMessage, "Generic Message", "`$1$t$2`", 0: "Left message", 1: "Right message");
 print_event!(IgnoreAdd, "Ignore Add", "`%O%C18$1%O added to ignore list.`", 0: "Hostmask");
 print_event!(IgnoreChanged, "Ignore Changed", "`%OIgnore on %C18$1%O changed.`", 0: "Hostmask");
-print_event!(IgnoreFooter, "Ignore Footer", "`%C16,17`", );
-print_event!(IgnoreHeader, "Ignore Header", "`%C16,17 Hostmask                  PRIV NOTI CHAN CTCP DCC  INVI UNIG`", );
+print_event!(IgnoreFooter, "Ignore Footer", "`%C16,17`",);
+print_event!(
+    IgnoreHeader,
+    "Ignore Header",
+    "`%C16,17 Hostmask                  PRIV NOTI CHAN CTCP DCC  INVI UNIG`",
+);
 print_event!(IgnoreRemove, "Ignore Remove", "`%O%C18$1%O removed from ignore list.`", 0: "Hostmask");
-print_event!(IgnorelistEmpty, "Ignorelist Empty", "`%OIgnore list is empty.`", );
+print_event!(
+    IgnorelistEmpty,
+    "Ignorelist Empty",
+    "`%OIgnore list is empty.`",
+);
 print_event!(Invite, "Invite", "`%C20*%O$tCannot join %C22$1%C (%C20Channel is invite only%O)`", 0: "Channel Name");
 print_event!(Invited, "Invited", "`%C24*%O$tYou have been invited to %C22$1%O by %C18$2%O (%C29$3%O)`", 0: "Channel Name", 1: "Nick of person who invited you", 2: "Server Name");
 print_event!(Join, "Join", "`%C23*$t$1 ($3%C23) has joined`", 0: "The nick of the joining person", 1: "The channel being joined", 2: "The host of the person", 3: "The account of the person");
@@ -87,22 +111,30 @@ print_event!(Kick, "Kick", "`%C22*%O$t%C26$1%C has kicked %C18$2%C from %C22$3%C
 print_event!(Killed, "Killed", "`%C19*%O$t%C19You have been killed by %C26$1%C (%C20$2%O)`", 0: "Nickname", 1: "Reason");
 print_event!(MessageSend, "Message Send", "`%O>%C18$1%C<%O$t$2`", 0: "Receiver", 1: "Message");
 print_event!(Motd, "Motd", "`%C29*%O$t%C29$1%O`", 0: "Text", 1: "Server Name", 2: "Raw Numeric or Identifier");
-print_event!(MotdSkipped, "MOTD Skipped", "`%C29*%O$t%C29MOTD Skipped%O`", );
+print_event!(MotdSkipped, "MOTD Skipped", "`%C29*%O$t%C29MOTD Skipped%O`",);
 print_event!(NickClash, "Nick Clash", "`%C23*%O$t%C28$1%C is already in use. Retrying with %C18$2%O...`", 0: "Nickname in use", 1: "Nick being tried");
 print_event!(NickErroneous, "Nick Erroneous", "`%C23*%O$t%C28$1%C is erroneous. Retrying with %C18$2%O...`", 0: "Nickname in use", 1: "Nick being tried");
-print_event!(NickFailed, "Nick Failed", "`%C20*%O$tNickname is erroneous or already in use. Use /NICK to try another.`", );
-print_event!(NoDcc, "No DCC", "`%C20*%O$tNo such DCC.`", );
-print_event!(NoRunningProcess, "No Running Process", "`%C23*%O$tNo process is currently running`", );
+print_event!(
+    NickFailed,
+    "Nick Failed",
+    "`%C20*%O$tNickname is erroneous or already in use. Use /NICK to try another.`",
+);
+print_event!(NoDcc, "No DCC", "`%C20*%O$tNo such DCC.`",);
+print_event!(
+    NoRunningProcess,
+    "No Running Process",
+    "`%C23*%O$tNo process is currently running`",
+);
 print_event!(Notice, "Notice", "`%O-%C18$1%O-$t$2`", 0: "Who it's from", 1: "The message");
 print_event!(NoticeSend, "Notice Send", "`%O->%C18$1%O<-$t$2`", 0: "Receiver", 1: "Message");
 print_event!(NotifyAway, "Notify Away", "`%C23*%O$tNotify: %C18$1%C is away (%C24$2%O)`", 0: "Nickname", 1: "Away Reason");
 print_event!(NotifyBack, "Notify Back", "`%C23*%O$tNotify: %C18$1%C is back`", 0: "Nickname", 1: "Server Name", 2: "Network");
-print_event!(NotifyEmpty, "Notify Empty", "`$tNotify list is empty.`", );
-print_event!(NotifyHeader, "Notify Header", "`%C16,17  Notify List`", );
+print_event!(NotifyEmpty, "Notify Empty", "`$tNotify list is empty.`",);
+print_event!(NotifyHeader, "Notify Header", "`%C16,17  Notify List`",);
 print_event!(NotifyNumber, "Notify Number", "`%C23*%O$t%C23$1%O users in notify list.`", 0: "Number of notify items");
 print_event!(NotifyOffline, "Notify Offline", "`%C23*%O$tNotify: %C18$1%C is offline (%C29$3%O)`", 0: "Nickname", 1: "Server Name", 2: "Network");
 print_event!(NotifyOnline, "Notify Online", "`%C23*%O$tNotify: %C18$1%C is online (%C29$3%O)`", 0: "Nickname", 1: "Server Name", 2: "Network");
-print_event!(OpenDialog, "Open Dialog", "``", );
+print_event!(OpenDialog, "Open Dialog", "``",);
 print_event!(Part, "Part", "`%C24*$t$1 ($2%C24) has left`", 0: "The nick of the person leaving", 1: "The host of the person", 2: "The channel");
 print_event!(PartWithReason, "Part with Reason", "`%C24*$t$1 ($2%C24) has left ($4)`", 0: "The nick of the person leaving", 1: "The host of the person", 2: "The channel", 3: "The reason");
 print_event!(PingReply, "Ping Reply", "`%C24*%O$tPing reply from %C18$1%C: %C24$2%O second(s)`", 0: "Who it's from", 1: "The time in x.x format (see below)");
@@ -111,14 +143,18 @@ print_event!(PrivateAction, "Private Action", "`%C18**$t$3$1%O $2 %C18**`", 0: "
 print_event!(PrivateActionToDialog, "Private Action to Dialog", "`%C18*$t$3$1%O $2`", 0: "Nickname", 1: "The message", 2: "Identified text");
 print_event!(PrivateMessage, "Private Message", "`%C18*%C18$3$1*%O$t$2`", 0: "Nickname", 1: "The message", 2: "Identified text");
 print_event!(PrivateMessageToDialog, "Private Message to Dialog", "`%C18%H<%H$3$1%H>%H%O$t$2`", 0: "Nickname", 1: "The message", 2: "Identified text");
-print_event!(ProcessAlreadyRunning, "Process Already Running", "`%C24*%O$tA process is already running`", );
+print_event!(
+    ProcessAlreadyRunning,
+    "Process Already Running",
+    "`%C24*%O$tA process is already running`",
+);
 print_event!(Quit, "Quit", "`%C24*$t$1 has quit ($2)`", 0: "Nick", 1: "Reason", 2: "Host");
 print_event!(RawModes, "Raw Modes", "`%C24*%O$t%C26$1%C sets modes %C30[%C24$2%C30]%O`", 0: "Nickname", 1: "Modes string");
 print_event!(ReceiveWallops, "Receive Wallops", "`%O-%C29$1/Wallops%O-$t$2`", 0: "Nickname", 1: "The message", 2: "Identified text");
 print_event!(ResolvingUser, "Resolving User", "`%C24*%O$tLooking up IP number for %C18$1%O...`", 0: "Nickname", 1: "Hostname");
 print_event!(SaslAuthenticating, "SASL Authenticating", "`%C23*%O$tAuthenticating via SASL as %C18$1%O (%C24$2%O)`", 0: "Username", 1: "Mechanism");
 print_event!(SaslResponse, "SASL Response", "`%C29*%O$t$4`", 0: "Server Name", 1: "Raw Numeric or Identifier", 2: "Username", 3: "Message");
-print_event!(ServerConnected, "Server Connected", "`%C29*%O$tConnected.`", );
+print_event!(ServerConnected, "Server Connected", "`%C29*%O$tConnected.`",);
 print_event!(ServerError, "Server Error", "`%C29*%O$t%C20$1%O`", 0: "Text");
 print_event!(ServerLookup, "Server Lookup", "`%C29*%O$tLooking up %C29$1%O`", 0: "Server Name");
 print_event!(ServerNotice, "Server Notice", "`%C29*%O$t$1`", 0: "Text", 1: "Server Name", 2: "Raw Numeric or Identifier");
@@ -128,7 +164,11 @@ print_event!(StopConnection, "Stop Connection", "`%C23*%O$tStopped previous conn
 print_event!(Topic, "Topic", "`%C22*%O$tTopic for %C22$1%C is: $2%O`", 0: "Channel", 1: "Topic");
 print_event!(TopicChange, "Topic Change", "`%C22*%O$t%C26$1%C has changed the topic to: $2%O`", 0: "Nick of person who changed the topic", 1: "Topic", 2: "Channel");
 print_event!(TopicCreation, "Topic Creation", "`%C22*%O$tTopic for %C22$1%C set by %C26$2%C (%C24$3%O)`", 0: "The channel", 1: "The creator", 2: "The time");
-print_event!(UnknownHost, "Unknown Host", "`%C20*%O$tUnknown host. Maybe you misspelled it?`", );
+print_event!(
+    UnknownHost,
+    "Unknown Host",
+    "`%C20*%O$tUnknown host. Maybe you misspelled it?`",
+);
 print_event!(UserLimit, "User Limit", "`%C20*%O$tCannot join %C22$1%C (%C20User limit reached%O)`", 0: "Channel Name");
 print_event!(UsersOnChannel, "Users On Channel", "`%C22*%O$tUsers on %C22$1%C: %C24$2%O`", 0: "Channel Name", 1: "Users");
 print_event!(WhoisAuthenticated, "WhoIs Authenticated", "`%C23*%O$t%C28[%C18$1%C28]%O $2 %C18$3%O`", 0: "Nickname", 1: "Message", 2: "Account");
